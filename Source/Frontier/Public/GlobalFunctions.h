42f5831eb91090ec611bf95fedebd9b832e1baf9
Anthony Cavuoti <anthonycavuoti@gmail.com>
2024-03-13 13:42:48 -0700

    Fix issues. Things are mostly like usual

---
+++ Source/Frontier/Public/GlobalFunctions.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GlobalFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTIER_API UGlobalFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
